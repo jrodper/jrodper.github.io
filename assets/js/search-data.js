a34f83dfe72b499eff02ff5cd4765396266bd6e6
jrodper <jorge.rodriguez@uva.es>
2025-11-05 22:57:02 +0000

    Deploying to gh-pages from @ jrodper/jrodper.github.io@d71ecbc4667c3ac7d7d3243d64286a4669b83080 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "CV",
-          description: "",
+          description: "For a PDF version of my CV contact me.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
